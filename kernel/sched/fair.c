4b4c699689a531bbf948da36dcfe7de974081e70
holyangel <slverwolf@gmail.com>
2016-06-12 12:03:26 -0500

    sched/fair: Reduce latency for better responsiveness
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    (cherry picked from commit 7d8cc7e87910630de72d554be44f55d339ac2d61)
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>
    (cherry picked from commit 820c081512b00d02ea4b163be106b20814d168f3)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -77,7 +77,7 @@ unsigned int normalized_sysctl_sched_min_granularity = 1250000ULL;
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 6;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -116,7 +116,7 @@ unsigned int __read_mostly sysctl_sched_shares_window = 10000000UL;
  *
  * default: 5 msec, units: microseconds
   */
-unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
+unsigned int sysctl_sched_cfs_bandwidth_slice = 4000UL;
 #endif
 
 /*
