d3b5110894de781bc6807f7bc85b098e3877e203
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-14 01:02:03 -0700

    cpuidle: Don't call no-op function to wake idle CPUs in pm_qos notifier
    There is no need to call a function when the only goal of sending the
    IPI in the pm_qos notifier is to wake idle CPUs. Skip the overhead by
    just sending the IPI and nothing else.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    (cherry picked from commit b15fbdec04b9c8f03717ffc83a2948f18d0ae505)
    Signed-off-by: Crash-Overlord <el_jose47@live.com>
    
    # Conflicts:
    #	drivers/cpuidle/cpuidle.c

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -658,22 +658,6 @@ int cpuidle_register(struct cpuidle_driver *drv,
 EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
-static atomic_t idle_cpu_mask = ATOMIC_INIT(0);
-
-#if NR_CPUS > 32
-#error idle_cpu_mask not big enough for NR_CPUS
-#endif
-
-static void cpuidle_set_idle_cpu(unsigned int cpu)
-{
-	atomic_or(BIT(cpu), &idle_cpu_mask);
-}
-
-static void cpuidle_clear_idle_cpu(unsigned int cpu)
-{
-	atomic_andnot(BIT(cpu), &idle_cpu_mask);
-}
-
 /*
  * This function gets called when a part of the kernel has a new latency
  * requirement.  This means we need to get only those processors out of their
@@ -683,17 +667,9 @@ static void cpuidle_clear_idle_cpu(unsigned int cpu)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	static unsigned long prev_latency[NR_CPUS] = {
-		[0 ... NR_CPUS - 1] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
-	};
-	unsigned long update_mask = 0;
-	unsigned int cpu;
-
-	/* Only send an IPI when the CPU latency requirement is tightened */
-	for_each_cpu(cpu, v) {
-		if (l < prev_latency[cpu])
-			update_mask |= BIT(cpu);
-		prev_latency[cpu] = l;
+	static unsigned long prev_latency = ULONG_MAX;
+	unsigned long cpus;
+	struct cpumask *idle_mask = to_cpumask(&cpus);
 	}
 
 	if (!update_mask)
