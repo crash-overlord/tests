171360c37f669507fe5949640ac43aa472a7dd01
crash-overlord <el_jose47@live.com>
2020-10-24 11:32:20 -0400

    Revert "msm: mdss: Boost only if last interaction was within 1s"
    This reverts commit 7deaabe42f27b7fd63a4e5a3200574c690419e47.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -10,8 +10,6 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 
-unsigned long last_input_time;
-
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -213,8 +211,6 @@ static void devfreq_boost_input_event(struct input_handle *handle,
 
 	for (i = 0; i < DEVFREQ_MAX; i++)
 		__devfreq_boost_kick(d->devices + i);
-
-	last_input_time = jiffies;
 }
 
 static int devfreq_boost_input_connect(struct input_handler *handler,
