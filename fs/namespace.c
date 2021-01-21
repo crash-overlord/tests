ad51bbac719bb0cc1da54ee4761369b4eb6f2438
Crash-Overlord <el_jose47@live.com>
2021-01-20 23:52:37 -0400

    testings
    Signed-off-by: Crash-Overlord <el_jose47@live.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2827,9 +2827,13 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 		goto dput_out;
 
 	/* Default to relatime unless overriden */
-//	if (!(flags & MS_NOATIME))
-//		mnt_flags |= MNT_RELATIME;
-
+	if (!(flags & MS_NOATIME))
+		mnt_flags |= MNT_RELATIME;
+	
+    /* Default to noatime/nodiratime unless overriden */
+	if (!(flags & MS_RELATIME))
+		mnt_flags |= MNT_NOATIME;
+    
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
 		mnt_flags |= MNT_NOSUID;
