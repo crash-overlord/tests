bb062711f0720c860103f20fbf6da07035521c32
Crash-Overlord <el_jose47@live.com>
2021-02-04 02:22:03 -0400

    ignore
    Signed-off-by: Crash-Overlord <el_jose47@live.com>

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 450 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#450 SMP PREEMPT Wed Feb 3 23:08:48 AST 2021"
-#define LINUX_COMPILE_BY "Crash-Overlord"
-#define LINUX_COMPILE_HOST "Crash-Pc"
-#define LINUX_COMPILER "gcc version 10.2.1 20201103 (GNU Toolchain for the A-profile Architecture 10.2-2020.11 (arm-10.16))"
