5dc4f47c1e7a0193fa6471e4f833344b1e5a5c19
Crash-Overlord <el_jose47@live.com>
2021-02-03 23:10:14 -0400

    added endif missed
    Signed-off-by: Crash-Overlord <el_jose47@live.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 450 */
 /* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#8 SMP PREEMPT Sun Jan 31 17:02:46 WET 2021"
-#define LINUX_COMPILE_BY "Zeetaa"
-#define LINUX_COMPILE_HOST "ZeetaaPrjkt"
-#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#450 SMP PREEMPT Wed Feb 3 23:08:48 AST 2021"
+#define LINUX_COMPILE_BY "Crash-Overlord"
+#define LINUX_COMPILE_HOST "Crash-Pc"
+#define LINUX_COMPILER "gcc version 10.2.1 20201103 (GNU Toolchain for the A-profile Architecture 10.2-2020.11 (arm-10.16))"
